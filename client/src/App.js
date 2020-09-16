6ed888379f1b3bcc393f45a5c876c4d854895010
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2020-09-15 21:35:21 -0400

    post base backend finish. front end start

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from './components/layout/Navbar'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { IconContext } from "react-icons";
+import { CgShapeHexagon } from "react-icons/cg";
+
+import "../styles/Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar_content_wrapper">
+        <div className="navbar_logo">
+          <IconContext.Provider value={{ className: "hexagon" }}>
+            <CgShapeHexagon />
+          </IconContext.Provider>{" "}
+          <span>JLC Studies</span>
+        </div>
+        <ul>
+          <li>Dashboard</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

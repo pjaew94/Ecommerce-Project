f10b97500cf998666ab56584323f71e622059eb9
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2020-10-13 16:18:21 -0400

    ready for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const connectDB = require('./config/db');
 
@@ -13,9 +14,15 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/subjects', require('./routes/api/subjects'))
 
-app.get('/', (req, res) => {
-    return res.send('API running');
-})
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.port || 5000;
 
